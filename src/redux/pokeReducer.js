ebacdc9cd862bf809165620a01e972eef5157193
jesusreyes01 <jesusareyes01@gmail.com>
2020-10-05 22:58:12 -0500

    completed lecture code

--- src/redux/pokeReducer.js
+++ src/redux/pokeReducer.js
@@ -1,32 +1,22 @@
 import axios from 'axios'
-
 //Create initial state
-
 const initialState = {
     pokemon: {}
-    
 }
-
 //Action creator/ Action Builder
 const GET_POKEMON = 'GET_POKEMON';
-
 export function getPokemon(){
     const rand = Math.ceil(Math.random() * 151);
     const pokemon = axios.get(`https://pokeapi.co/api/v2/pokemon/${rand}`)
-
-
     return{
         //Actions always have a type a payload property
         type: GET_POKEMON,
-        payload: pokemon
-       
+        payload: pokemon 
     }
 }
-
 //Reducer function, control center for your reducer, is what acutally state in redux
 export default function pokeReducer(state = initialState, action){
     const {type, payload} = action;
-
     //Switch determines which action invoked, and then changes state
     switch(type){
         case GET_POKEMON + '_PENDING': 
@@ -37,6 +27,5 @@ export default function pokeReducer(state = initialState, action){
             return state;
         default:
             return state;
-
     }
 }
